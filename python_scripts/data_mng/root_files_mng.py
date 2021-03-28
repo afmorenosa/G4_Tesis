64b49e8c634142d40e9eb0e785cd4e9f4da6a968
afmorenosa <afmorenosa@unal.edu.co>
2021-03-28 14:50:06 -0500

    Improve machine learning traning implementation

---
+++ python_scripts/data_mng/root_files_mng.py
@@ -0,0 +1,149 @@
+"""Function that manage root files."""
+from ROOT import TFile
+import numpy as np
+
+
+def get_nentries(files_list):
+    """Get the total number of entries in the root files.
+
+    Args:
+    ----
+        files_list: Tupple or list with string of the paths to the
+        root files.
+
+    Retrutns:
+    --------
+        The total nomber of entries in the given root files.
+
+    """
+    # Set .
+    nentries = 0
+
+    for file_name in files_list:
+        # Open root file.
+        root_file = TFile.Open(file_name)
+
+        # Get Photons TTree.
+        photons_branch = root_file.GetDirectory("ntuple").Get("Photons")
+
+        # Get number of non-empty entries.
+        for entry in photons_branch:
+            if len(entry.X):
+                nentries += 1
+
+    return nentries
+
+
+def get_data(files_list, nentries, x_array_file="x_temp.data",
+             y_array_file="y_temp.data"):
+    """Extract data from a set of files.
+
+    Args:
+    ----
+        files_list: Tupple or list with string of the paths to the
+        root files.
+        nentries: Int of the total nomber of entries in the given root files.
+        x_array_file: String of the file where the X data is stored.
+        y_array_file: String of the file where the y data is stored.
+
+    """
+    # Create the variables to store the data.
+    X_set = np.memmap(x_array_file, mode="w+",
+                      shape=(nentries, 77184))
+    y_set = np.memmap(y_array_file, mode="w+", shape=(nentries))
+
+    index = 0
+
+    for file_name in files_list:
+
+        # Open root file.
+        root_file = TFile.Open(file_name)
+
+        # Get Photons TTree.
+        photons_branch = root_file.GetDirectory("ntuple").Get("Photons")
+
+        i = 0
+        for entry in photons_branch:
+
+            # Fill the data of the TTree.
+            i += 1
+
+            # Print percentage of progress.
+            print(f"[{i/photons_branch.GetEntries()*100:.2f}%]",
+                  f"getting data from: {file_name} - entry: {i}, of: " +
+                  f"{photons_branch.GetEntries()}",
+                  f"Non-emtpy data: {index}, of: {nentries}",
+                  end="\r")
+
+            if len(entry.X) == 0:
+                continue
+            photons_counter = np.zeros((16*3, 8*3, 67))
+
+            # Set a zeros matrix.
+            for x in entry.X:
+                for y in entry.Y:
+                    for z in entry.Z:
+                        photons_counter[x, y, z] += 1
+            # Add data.
+            X_set[index] = photons_counter.flatten()
+            y_set[index] = entry.primary
+
+            index += 1
+
+        # Close root file.
+        root_file.Close()
+        print()
+
+
+def train_data(files_list, classification_method):
+    """Train a classifier over a set of files.
+
+    Args:
+    ----
+        files_list: Tupple or list with string of the paths to the
+        root files.
+        classification_method: Classification method to be trained. It should
+        have partial_fit method.
+
+    """
+    for file_name in files_list:
+        X_set = []
+        y_set = []
+
+        # Open root file.
+        root_file = TFile.Open(file_name)
+
+        # Get Photons TTree.
+        photons_branch = root_file.GetDirectory("ntuple").Get("Photons")
+
+        i = 0
+        for entry in photons_branch:
+
+            # Fill the data of the TTree.
+            i += 1
+
+            # Print percentage of progress.
+            print(f"[{i/photons_branch.GetEntries()*100:.2f}%]",
+                  f"Getting data from: {file_name} - entry: {i}, of: " +
+                  f"{photons_branch.GetEntries()}",
+                  end="\r")
+
+            if len(entry.X) == 0:
+                continue
+            photons_counter = np.zeros((16*3, 8*3, 67))
+
+            # Set a zeros matrix.
+            for x in entry.X:
+                for y in entry.Y:
+                    for z in entry.Z:
+                        photons_counter[x, y, z] += 1
+            # Add data.
+            X_set.append(photons_counter.flatten())
+            y_set.append(entry.primary)
+
+        # Train the classifier.
+        classification_method.partial_fit(X_set, y_set, classes=[0, 1, 2])
+
+        # Close root file.
+        root_file.Close()
+        print()
--- python_scripts/ia_train.py
+++ python_scripts/ia_train.py
@@ -4,8 +4,12 @@ Train machine learning.
 Script that take data from the root files and train a machine learning with
 scikitlearn lib.
 """
-from sklearn.neural_network import MLPClassifier
-from ROOT import TFile
+# from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+# from sklearn.linear_model import Perceptron
+# from sklearn.linear_model import SGDClassifier
+# from sklearn.linear_model import PassiveAggressiveClassifier
+from data_mng import root_files_mng
 import numpy as np
 import argparse
 
@@ -16,141 +20,33 @@ parser = argparse.ArgumentParser(description='Train and machine learning.')
 parser.add_argument("-tr", "--train", type=str, metavar="train_file",
                     nargs="+",
                     required=True,
-                    help="The input root file to train the machine learning")
+                    help="The input root file to train the machine learning.")
 
 parser.add_argument("-ts", "--test", type=str, metavar="test_file", nargs="+",
                     required=True,
-                    help="The input root file to test the machine learning")
+                    help="The input root file to test the machine learning.")
 
 # Parse arguments.
 args = parser.parse_args()
 
-
-def get_nentries(files_list):
-    """Get the total number of entries in the root files.
-
-    Args:
-    ----
-        files_list: Tupple or list with string of the paths to the
-        root files.
-
-    Retrutns:
-    --------
-        The total nomber of entries in the given root files.
-
-    """
-    # Set .
-    nentries = 0
-
-    for file_name in files_list:
-        # Open root file.
-        root_file = TFile.Open(file_name)
-
-        # Get Photons TTree.
-        photons_branch = root_file.GetDirectory("ntuple").Get("Photons")
-
-        # Get number of non-empty entries.
-        for entry in photons_branch:
-            if len(entry.X):
-                nentries += 1
-
-    return nentries
-
-
-def get_data(files_list, nentries, array_file="temp.dat"):
-    """Extract data from a set of files.
-
-    Args:
-    ----
-        files_list: Tupple or list with string of the paths to the
-        root files.
-        nentries: Int of the total nomber of entries in the given root files.
-        array_file: String of the file where the data is stored.
-
-    Retrutns:
-    --------
-        Two lists with the data of the number of the photons created in each
-        scintillator and the primary particle.
-
-    """
-    # Create the variables to store the data.
-    X_set = np.memmap(array_file, dtype=np.int64, mode="w+",
-                      shape=(nentries, 77184))
-    y_set = []
-
-    index = 0
-
-    for file_name in files_list:
-
-        # Open root file.
-        root_file = TFile.Open(file_name)
-
-        # Get Photons TTree.
-        photons_branch = root_file.GetDirectory("ntuple").Get("Photons")
-
-        i = 0
-        for entry in photons_branch:
-
-            # Fill the data of the TTree.
-            i += 1
-
-            # Print percentage of progress.
-            print(f"[{i/photons_branch.GetEntries()*100:.2f}%]",
-                  f"gettin data from: {file_name} - entry: {i}, of: " +
-                  f"{photons_branch.GetEntries()}",
-                  f"Non-emtpy data: {index}, of: {nentries}",
-                  end="\r")
-
-            if len(entry.X) == 0:
-                continue
-
-            photons_counter = np.zeros((16*3, 8*3, 67))
-
-            # Set a zeros matrix.
-            for x in entry.X:
-                for y in entry.Y:
-                    for z in entry.Z:
-                        photons_counter[x, y, z] += 1
-            # Add data.
-            X_set[index] = photons_counter.flatten()
-            y_set.append(entry.primary)
-
-            index += 1
-
-        # Close root file.
-        root_file.Close()
-        print()
-
-    # Return data sets.
-    return X_set, y_set
-
-
 # Create the classifiers.
-clf = MLPClassifier(solver='adam', alpha=1e-5,
-                    hidden_layer_sizes=(5, 7, 9, 4, 3), random_state=1)
-
-# Get the number of entries.
-nentries = get_nentries(args.train)
-
-# Get the training variables.
-X_train, y_train = get_data(args.train, nentries)
-
+clf = BernoulliNB()
 # Train the data.
-print("\nTraining", f"Total of data: {len(X_train)}")
-clf.fit(X_train, y_train)
+print("\nTraining")
+root_files_mng.train_data(args.train, clf)
 print("Trained\n")
 
-# Delete train data.
-del X_train, y_train
-
 # Get the number of entries.
-nentries = get_nentries(args.test)
+nentries = root_files_mng.get_nentries(args.test)
 
 # Get the test variables.
-X_test, y_test = get_data(args.test, nentries)
+root_files_mng.get_data(args.test, nentries)
+X_test = np.memmap("x_temp.data", shape=(nentries, 77184))
+y_test = np.memmap("y_temp.data", shape=(nentries))
 
 # Print results of the test.
 print("\n")
 print(f"Results {clf.score(X_test, y_test)}")
 
+# Delete train data.
 del X_test, y_test

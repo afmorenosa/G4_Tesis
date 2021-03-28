072fecd57cbce2ba528f7e951bad4acdb0790723
afmorenosa <afmorenosa@unal.edu.co>
2021-03-28 15:23:26 -0500

    Configure parser to accept the classifier as an input

--- python_scripts/ia_train.py
+++ python_scripts/ia_train.py
@@ -4,17 +4,18 @@ Train machine learning.
 Script that take data from the root files and train a machine learning with
 scikitlearn lib.
 """
-# from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
-# from sklearn.linear_model import Perceptron
-# from sklearn.linear_model import SGDClassifier
-# from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import PassiveAggressiveClassifier
 from data_mng import root_files_mng
 import numpy as np
 import argparse
 
 # Set parser for the console arguments.
-parser = argparse.ArgumentParser(description='Train and machine learning.')
+parser = argparse.ArgumentParser(description='Train and machine learning.',
+                                 formatter_class=argparse.RawTextHelpFormatter)
 
 # Set arguments.
 parser.add_argument("-tr", "--train", type=str, metavar="train_file",
@@ -26,11 +27,26 @@ parser.add_argument("-ts", "--test", type=str, metavar="test_file", nargs="+",
                     required=True,
                     help="The input root file to test the machine learning.")
 
+parser.add_argument("-c", "--classifier", type=str,
+                    metavar="classifier_method", default="MultinomialNB",
+                    help="""
+The machine learning classifier to be used.
+You can select between MultinomialNB, BernoulliNB,
+Perceptron, SGDClassifier and PassiveAggressiveClassifier.
+(default: MultinomialNB).""")
+
 # Parse arguments.
 args = parser.parse_args()
 
+classifiers_dict = {"MultinomialNB": MultinomialNB, "BernoulliNB": BernoulliNB,
+                    "Perceptron": Perceptron,
+                    "SGDClassifier": SGDClassifier,
+                    "PassiveAggressiveClassifier": PassiveAggressiveClassifier}
+
 # Create the classifiers.
-clf = BernoulliNB()
+print(f"\nUsing {args.classifier} classifier")
+clf = classifiers_dict[args.classifier]()
+
 # Train the data.
 print("\nTraining")
 root_files_mng.train_data(args.train, clf)

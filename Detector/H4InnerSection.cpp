2b0a5f003d7e02462c79b0fe51285fe7f0ac0966
ccmorenosa <afmorenosa@unal.edu.co>
2022-02-28 18:48:55 -0500

    Improve plates creation in the inner module

--- Detector/H4InnerSection.cpp
+++ Detector/H4InnerSection.cpp
@@ -7,7 +7,15 @@
 //       Do nothing.          //
 //                            //
 //****************************//
-H4InnerSection::H4InnerSection () : H4Module() {}
+H4InnerSection::H4InnerSection (
+  G4Material *lead_mat,
+  G4Material *aerog_mat,
+  G4Material *wls_mat
+) : H4Module(lead_mat, aerog_mat, wls_mat) {
+
+  BuildPlates();
+
+}
 
 //****************************//
 // H4InnerSection destructor  //
@@ -17,6 +25,134 @@ H4InnerSection::H4InnerSection () : H4Module() {}
 //                            //
 //****************************//
 H4InnerSection::~H4InnerSection () {}
+
+//*********************************//
+//                                 //
+// Build the holes for the plates. //
+//                                 //
+//*********************************//
+void H4InnerSection::BuildPlates (
+  G4double radius,
+  G4double pb_thickness,
+  G4double sc_thickness
+) {
+
+  m_pb_hole = new G4Tubs(
+    "Lead Hole",
+    0.,
+    radius,
+    pb_thickness,
+    0.,
+    360.*deg
+  );
+
+  m_sc_hole = new G4Tubs(
+    "Scintillator_Plate Hole",
+    0.,
+    radius,
+    sc_thickness,
+    0.,
+    360.*deg
+  );
+
+  // Main box.
+  G4VSolid *pb_main_box = new G4Box(
+    "Lead Main Box", 6.06*cm, 6.06*cm, pb_thickness
+  );
+
+  G4Transform3D transform;
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+
+      transform = G4Transform3D(
+        G4RotationMatrix(0, 0, 0),
+        G4ThreeVector((i-1) * 4.04 * cm, (j-1) * 4.04 * cm, 0.0)
+      );
+
+      pb_main_box = new G4SubtractionSolid(
+        "Lead Main Box solid",
+        pb_main_box,
+        m_pb_hole,
+        transform
+      );
+
+      for (int I = 0; I < 4; I++) {
+        for (int J = 0; J < 4; J++) {
+
+          transform = G4Transform3D(
+            G4RotationMatrix(0, 0, 0),
+            G4ThreeVector((i-1) * 4.04 * cm, (j-1) * 4.04 * cm, 0.0) +
+            G4ThreeVector(
+              -2.02*cm + (0.5 + I) * 1.01*cm,
+              -2.02*cm + (0.5 + J) * 1.01*cm,
+              0
+            )
+          );
+
+          pb_main_box = new G4SubtractionSolid(
+            "Lead Main Box solid",
+            pb_main_box,
+            m_pb_hole,
+            transform
+          );
+
+        }
+      }
+    }
+  }
+
+  m_pb_plate_log = new G4LogicalVolume(
+    pb_main_box,
+    m_lead_mat,
+    "Lead plate"
+  );
+
+  // Main box.
+  G4VSolid *sc_main_box = new G4Box(
+    "Scintillator Main Box", 2.02*cm, 2.02*cm, sc_thickness
+  );
+
+  transform = G4Transform3D(
+    G4RotationMatrix(0, 0, 0), G4ThreeVector(0., 0., 0.)
+  );
+
+  sc_main_box = new G4SubtractionSolid(
+    "Scintillator Main Box solid",
+    sc_main_box,
+    m_sc_hole,
+    transform
+  );
+
+  for (int I = 0; I < 4; I++) {
+    for (int J = 0; J < 4; J++) {
+
+      transform = G4Transform3D(
+        G4RotationMatrix(0, 0, 0),
+        G4ThreeVector(
+          -2.02*cm + (0.5 + I) * 1.01*cm,
+          -2.02*cm + (0.5 + J) * 1.01*cm,
+          0
+        )
+      );
+
+      sc_main_box = new G4SubtractionSolid(
+        "Scintillator Main Box solid",
+        sc_main_box,
+        m_sc_hole,
+        transform
+      );
+
+    }
+  }
+
+  m_sc_plate_log = new G4LogicalVolume(
+    sc_main_box,
+    m_aerog_mat,
+    "Scintillator plate"
+  );
+
+}
 
 //***************************************//
 //                                       //
@@ -40,20 +176,11 @@ void H4InnerSection::PlaceHolePlate (
   // Lead tale.
   if (!is_scintillator) {
 
-    // Main box.
-    G4Box *main_box = new G4Box("Main Box", 6.06*cm, 6.06*cm, thickness);
-
-    G4LogicalVolume *plate_log = new G4LogicalVolume(
-      main_box,
-      plate_mat,
-      log_name
-    );
-
     // Place the volume of the plate.
     new G4PVPlacement(
       rot,
       tlate,
-      plate_log,
+      m_pb_plate_log,
       name,
       mother_logical,
       many,
@@ -64,21 +191,12 @@ void H4InnerSection::PlaceHolePlate (
     return;
   }
 
-  // Main box.
-  G4Box *main_box = new G4Box("Main Box", 2.02*cm, 2.02*cm, thickness);
-
-  G4LogicalVolume *plate_log = new G4LogicalVolume(
-    main_box,
-    plate_mat,
-    log_name
-  );
-
   // Place the volume of the plate.
   new G4PVPlacement(
     rot,
     tlate,
-    plate_log,
-    name,
+    m_sc_plate_log,
+    G4String(name),
     mother_logical,
     many,
     copy_no,
@@ -109,9 +227,10 @@ void H4InnerSection::PlaceLargePlate (
 
   // Place the lead tile.
   if (!is_scintillator) {
+
     PlaceHolePlate (
       is_scintillator,
-      plate_mat,
+      m_lead_mat,
       thickness,
       rot,
       tlate,
@@ -132,7 +251,7 @@ void H4InnerSection::PlaceLargePlate (
 
       PlaceHolePlate (
         is_scintillator,
-        plate_mat,
+        m_aerog_mat,
         thickness,
         rot,
         tlate + G4ThreeVector((i-1) * 4.04 * cm, (j-1) * 4.04 * cm, 0.0),
@@ -168,7 +287,7 @@ void H4InnerSection::AddWLS (
 ) {
 
   // Wire tube.
-  G4Tubs *wire_tube = new G4Tubs("Hole", 0, .6*mm,
+  G4Tubs *wire_tube = new G4Tubs("Wire Tube", 0, .6*mm,
   sc_thickness * 67 + pb_thickness * 66, 0.*deg, 360.*deg);
 
   // Wire logical volume.

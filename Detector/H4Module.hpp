ec7268ca03319288c5b0ca93115f73e5b3baa478
ccmorenosa <afmorenosa@unal.edu.co>
2022-03-01 16:54:00 -0500

    Deprecate unused variables

--- Detector/H4Module.hpp
+++ Detector/H4Module.hpp
@@ -35,23 +35,18 @@ public:
     G4LogicalVolume *mother_logical,
     G4double pb_thickness = 1*mm,
     G4double sc_thickness = 2*mm,
-    const G4String &pb_log_name = "Lead_Plate",
-    const G4String &sc_log_name = "Scintillator_Plate",
     G4bool many = false,
     G4int copy_no = 0,
     G4bool surf_chk = true
   );
 
   void PlacePbScPlates (
-    G4Material *sc_plate_mat,
     G4RotationMatrix *rot,
     const G4ThreeVector &tlate,
     const G4String &name,
     G4LogicalVolume *mother_logical,
     G4double pb_thickness = 1*mm,
     G4double sc_thickness = 2*mm,
-    const G4String &pb_log_name = "Lead_Plate",
-    const G4String &sc_log_name = "Scintillator_Plate",
     G4bool many = false,
     G4int copy_no = 0,
     G4bool surf_chk = true
@@ -87,28 +82,11 @@ private:
     G4double sc_thickness = 2*mm
   ) = 0;
 
-  virtual void PlaceHolePlate (
-    G4bool is_scintillator,
-    G4Material *plate_mat,
-    G4double thickness,
-    G4RotationMatrix *rot,
-    const G4ThreeVector &tlate,
-    const G4String &name,
-    const G4String &log_name,
-    G4LogicalVolume *mother_logical,
-    G4bool many = false,
-    G4int copy_no = 0,
-    G4bool surf_chk = true
-  ) = 0;
-
   virtual void PlaceLargePlate (
     G4bool is_scintillator,
-    G4Material *plate_mat,
-    G4double thickness,
     G4RotationMatrix *rot,
     const G4ThreeVector &tlate,
     const G4String &name,
-    const G4String &log_name,
     G4LogicalVolume *mother_logical,
     G4bool many = false,
     G4int copy_no = 0,
@@ -116,7 +94,6 @@ private:
   ) = 0;
 
   virtual void AddWLS (
-    G4Material *wls_mat,
     G4RotationMatrix *rot,
     const G4ThreeVector &tlate,
     const G4String &name,
